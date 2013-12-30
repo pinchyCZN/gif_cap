198587056de51631ec62dd5a281fc6be39f9423a
pinchyCZN <mail2@crazynation.org>
2013-12-29 21:15:19 -0600

    updates

--- gif_cap.c
+++ gif_cap.c
@@ -4,6 +4,7 @@
 
 HINSTANCE ghinstance=0;
 HINSTANCE ghprevinstance=0;
+HWND ghdlg=0;
 int assert(int a)
 {
 	if(!a){
@@ -110,6 +111,185 @@ int clear_window(HWND hwnd,HRGN *hregion)
 	}
 	set_title(hwnd);
 }
+
+PBITMAPINFO CreateBitmapInfoStruct(HBITMAP hBmp)
+{ 
+	BITMAP bmp; 
+	PBITMAPINFO pbmi; 
+	WORD    cClrBits;
+	// Retrieve the bitmap color format, width, and height. 
+	if (!GetObject(hBmp, sizeof(BITMAP), (LPSTR)&bmp)) 
+		return NULL;
+	
+	// Convert the color format to a count of bits. 
+	cClrBits = (WORD)(bmp.bmPlanes * bmp.bmBitsPixel); 
+	if (cClrBits == 1) 
+		cClrBits = 1; 
+	else if (cClrBits <= 4) 
+		cClrBits = 4; 
+	else if (cClrBits <= 8) 
+		cClrBits = 8; 
+	else if (cClrBits <= 16) 
+		cClrBits = 16; 
+	else if (cClrBits <= 24) 
+		cClrBits = 24; 
+	else cClrBits = 32; 
+	
+	// Allocate memory for the BITMAPINFO structure. (This structure 
+	// contains a BITMAPINFOHEADER structure and an array of RGBQUAD 
+	// data structures.) 
+	
+	if (cClrBits != 24) 
+		pbmi = (PBITMAPINFO) LocalAlloc(LPTR, 
+		sizeof(BITMAPINFOHEADER) + 
+		sizeof(RGBQUAD) * (1<< cClrBits)); 
+	
+	// There is no RGBQUAD array for the 24-bit-per-pixel format. 
+	
+	else 
+		pbmi = (PBITMAPINFO) LocalAlloc(LPTR, 
+		sizeof(BITMAPINFOHEADER)); 
+	
+	// Initialize the fields in the BITMAPINFO structure. 
+	
+	pbmi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER); 
+	pbmi->bmiHeader.biWidth = bmp.bmWidth; 
+	pbmi->bmiHeader.biHeight = bmp.bmHeight; 
+	pbmi->bmiHeader.biPlanes = bmp.bmPlanes; 
+	pbmi->bmiHeader.biBitCount = bmp.bmBitsPixel; 
+	if (cClrBits < 24) 
+		pbmi->bmiHeader.biClrUsed = (1<<cClrBits); 
+	
+	// If the bitmap is not compressed, set the BI_RGB flag. 
+	pbmi->bmiHeader.biCompression = BI_RGB; 
+	
+	// Compute the number of bytes in the array of color 
+	// indices and store the result in biSizeImage. 
+	// For Windows NT, the width must be DWORD aligned unless 
+	// the bitmap is RLE compressed. This example shows this. 
+	// For Windows 95/98/Me, the width must be WORD aligned unless the 
+	// bitmap is RLE compressed.
+	pbmi->bmiHeader.biSizeImage = ((pbmi->bmiHeader.biWidth * cClrBits +31)& ~31) /8
+		* pbmi->bmiHeader.biHeight; 
+	// Set biClrImportant to 0, indicating that all of the 
+	// device colors are important. 
+	pbmi->bmiHeader.biClrImportant = 0; 
+	return pbmi; 
+} 
+void CreateBMPFile(LPTSTR pszFile, PBITMAPINFO pbi, 
+				   HBITMAP hBMP, HDC hDC) 
+{ 
+	HANDLE hf;                 // file handle 
+	BITMAPFILEHEADER hdr;       // bitmap file-header 
+	PBITMAPINFOHEADER pbih;     // bitmap info-header 
+	LPBYTE lpBits;              // memory pointer 
+	DWORD dwTotal;              // total count of bytes 
+	DWORD cb;                   // incremental count of bytes 
+	BYTE *hp;                   // byte pointer 
+	DWORD dwTmp; 
+	
+	pbih = (PBITMAPINFOHEADER) pbi; 
+	lpBits = (LPBYTE) GlobalAlloc(GMEM_FIXED, pbih->biSizeImage);
+	
+	if (!lpBits) 
+		return; 
+	
+	// Retrieve the color table (RGBQUAD array) and the bits 
+	// (array of palette indices) from the DIB. 
+	if (!GetDIBits(hDC, hBMP, 0, (WORD) pbih->biHeight, lpBits, pbi, 
+		DIB_RGB_COLORS)) 
+	{
+		return;
+	}
+	
+	// Create the .BMP file. 
+	hf = CreateFile(pszFile, 
+		GENERIC_READ | GENERIC_WRITE, 
+		(DWORD) 0, 
+		NULL, 
+		CREATE_ALWAYS, 
+		FILE_ATTRIBUTE_NORMAL, 
+		(HANDLE) NULL); 
+	if (hf == INVALID_HANDLE_VALUE) 
+		return; 
+	hdr.bfType = 0x4d42;        // 0x42 = "B" 0x4d = "M" 
+	// Compute the size of the entire file. 
+	hdr.bfSize = (DWORD) (sizeof(BITMAPFILEHEADER) + 
+		pbih->biSize + pbih->biClrUsed 
+		* sizeof(RGBQUAD) + pbih->biSizeImage); 
+	hdr.bfReserved1 = 0; 
+	hdr.bfReserved2 = 0; 
+	
+	// Compute the offset to the array of color indices. 
+	hdr.bfOffBits = (DWORD) sizeof(BITMAPFILEHEADER) + 
+		pbih->biSize + pbih->biClrUsed 
+		* sizeof (RGBQUAD); 
+	
+	// Copy the BITMAPFILEHEADER into the .BMP file. 
+	if (!WriteFile(hf, (LPVOID) &hdr, sizeof(BITMAPFILEHEADER), 
+		(LPDWORD)&dwTmp,  NULL)) 
+	{
+		return; 
+	}
+	
+	// Copy the BITMAPINFOHEADER and RGBQUAD array into the file. 
+	if (!WriteFile(hf, (LPVOID) pbih, sizeof(BITMAPINFOHEADER) 
+		+ pbih->biClrUsed * sizeof (RGBQUAD), 
+		(LPDWORD) &dwTmp, ( NULL))) 
+		return; 
+	
+	// Copy the array of color indices into the .BMP file. 
+	dwTotal = cb = pbih->biSizeImage; 
+	hp = lpBits; 
+	if (!WriteFile(hf, (LPSTR) hp, (int) cb, (LPDWORD) &dwTmp,NULL)) 
+		return; 
+	
+	// Close the .BMP file. 
+	if (!CloseHandle(hf)) 
+		return; 
+	
+	// Free memory. 
+	GlobalFree((HGLOBAL)lpBits);
+}
+int take_snap(HWND hwnd)
+{
+	HDC hdc;
+	hdc=GetDC(NULL);
+	if(hdc!=0){
+		HDC hcomp;
+		hcomp=CreateCompatibleDC(hdc);
+		if(hcomp!=0){
+			RECT rect;
+			HBITMAP hbmp;
+			int w,h;
+			GetWindowRect(GetDesktopWindow(),&rect);
+			w=rect.right-rect.left;
+			h=rect.bottom-rect.top;
+			hbmp=CreateCompatibleBitmap(hdc,w,h);
+			if(hbmp!=0){
+				PBITMAPINFO pBitmapInfo;
+				HBITMAP hrpl;
+				pBitmapInfo=CreateBitmapInfoStruct(hbmp);
+				hrpl=SelectObject(hcomp,hbmp);
+				CreateBMPFile(("b:\\picture.bmp"), pBitmapInfo, hbmp, hcomp);
+				/*
+				BitBlt(hcomp,0,0,w,h,hdc,0,0,SRCCOPY);
+				*/
+
+				//copy to clipboard
+				OpenClipboard(NULL);
+				EmptyClipboard();
+				SetClipboardData(CF_BITMAP, hbmp);
+				CloseClipboard();
+
+				DeleteObject(hbmp);
+			}
+			DeleteDC(hcomp);
+		}
+		ReleaseDC(NULL,hdc);
+	}
+
+}
 BOOL CALLBACK gifcap(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 {
 	static HRGN hregion=0;
@@ -121,6 +301,7 @@ BOOL CALLBACK gifcap(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 	case WM_INITDIALOG:
 		{
 			RECT rect={0};
+			ghdlg=hwnd;
 			center_window_to_desk(hwnd);
 			GetWindowRect(hwnd,&rect);
 			hregion=CreateRectRgn(0,0,rect.right-rect.left,rect.bottom-rect.top);
@@ -130,6 +311,12 @@ BOOL CALLBACK gifcap(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 		}
 		break;
 	case WM_APP:
+		switch(wparam){
+		case '1':
+			take_snap(hwnd);
+			printf("made it\n");
+			break;
+		}
 		break;
 	case WM_SIZE:
 		clear_window(hwnd,&hregion);
@@ -154,8 +341,10 @@ int key_thread()
 {
 	while(TRUE){
 		Sleep(100);
-		if(GetAsyncKeyState('1')&0x8001)
+		if(GetAsyncKeyState('1')&0x8001){
+			PostMessage(ghdlg,WM_APP,'1',0);
 			printf("1 presed\n");
+		}
 	}
 }
 int CALLBACK WinMain(HINSTANCE hinstance,HINSTANCE hprevinstance,char *cmdline,int showcmd)
