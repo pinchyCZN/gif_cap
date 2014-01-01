816109d694541d612a37b7bee194a89ff40b3380
pinchyCZN <mail2@crazynation.org>
2013-12-31 21:59:21 -0600

    updates

--- gif_cap.c
+++ gif_cap.c
@@ -252,132 +252,79 @@ void CreateBMPFile(LPTSTR pszFile, PBITMAPINFO pbi,
 	GlobalFree((HGLOBAL)lpBits);
 }
 static int colortable[256*3+1];
-static int table_len=0;
-int dither(unsigned char *data,int w,int h,int row_width,unsigned char *pixels)
+int downsample(unsigned char *data,int w,int h,int row_width,unsigned char *pixels)
 {
 	if(pixels!=0){
-		int i,j,k,poffset=0;
-		int y=0;
-		for(i=0;i<h;i++){
+		int i,j,poffset=0;
+		for(i=h-1;i>=0;i--){
 			for(j=0;j<w;j++){
-				int x=y+j*4;
-				int index=0;
-				for(k=0;k<table_len;k++){
-					unsigned char r,g,b;
-					b=data[x+0]&0xE0;
-					g=data[x+1]&0xE0;
-					r=data[x+2]&0xC0;
-					if(colortable[k]==r && colortable[k+1]==g && colortable[k+2]==b){
-						index=k;
-						break;
-					}
-				}
+				int x=(i*row_width)+(j*4);
+				unsigned char index=0;
+				unsigned char r,g,b;
+				b=data[x+0]&0xE0;
+				g=data[x+1]&0xE0;
+				r=data[x+2]&0xC0;
+				index=r|(g>>2)|(b>>6);
 				pixels[poffset++]=index;
 			}
-			y+=row_width;
 		}
 	}
 }
-int create_ctable(unsigned char *data,int w,int h,int len)
+int create_ctable()
 {
-	int i,j,k,index=0,c=0;
-	int modulo;
-	j=h;
-	if(j==0)
-		j=1;
-	modulo=len/j;
-	for(i=0;i<h;i++){
-		for(j=0;j<w;j++){
-			unsigned char r,g,b;
-			int found=FALSE;
-			int x=i*modulo+(j*4);
-			b=data[x+0]&0xE0;
-			g=data[x+1]&0xE0;
-			r=data[x+2]&0xC0;
-			if(index<256){
-				for(k=0;k<index;k+=3){
-					if(colortable[k]==r && colortable[k+1]==g && colortable[k+2]==b){
-						found=TRUE;
-						break;
-					}
-				}
-				if(!found){
-					colortable[index*3]=r;
-					colortable[index*3+1]=g;
-					colortable[index*3+2]=b;
-					index++;
-				}
-			}
-			else
-				break;
-		}
-	}
-	for(i=index;i<256;i++){
-		colortable[index*3]=0;
-		colortable[index*3+1]=0;
-		colortable[index*3+2]=0;
+	int i,index;
+	static int created=FALSE;
+	if(created)
+		return TRUE;
+	index=0;
+	for(i=0;i<256;i++){
+		unsigned char r,g,b;
+		r=i>>6;
+		g=(i>>3)&0x7;
+		b=i&3;
+		colortable[index*3]=r<<6;
+		colortable[index*3+1]=g<<5;
+		colortable[index*3+2]=b<<5;
 		index++;
 	}
-	table_len=index;
 	colortable[index*3]=-1;
+	created=TRUE;
+	return TRUE;
 }
-int save_gif(HDC hdc,HBITMAP hbmp,BITMAPINFO *bmp_info)
+int grab_pixels(HDC hdc,HBITMAP hbmp,BITMAP *bmp,unsigned char **pixels,int w,int h)
 {
 	int result=FALSE;
 	unsigned char *data;
-	BITMAPINFOHEADER *bmp_header;
+	BITMAPINFO bmi={0};
 
-	bmp_header=bmp_info;
-	data=GlobalAlloc(GMEM_FIXED,bmp_header->biSizeImage);
+	data=GlobalAlloc(GMEM_FIXED,bmp->bmWidthBytes*bmp->bmHeight);
 	
 	if (!data) 
-		return result; 
-	if(GetDIBits(hdc,hbmp,0,(WORD)bmp_header->biHeight,data,bmp_info,DIB_RGB_COLORS)){
-		unsigned char *gifimage=0;
-		unsigned char *pixels=0;
-		void *gsdata=0;
-		int bgindex=0;
-		create_ctable(data,bmp_header->biWidth,bmp_header->biHeight,bmp_header->biSizeImage);
+		return result;
+	bmi.bmiHeader.biSize=sizeof(BITMAPINFOHEADER);
+	bmi.bmiHeader.biBitCount=32;
+	bmi.bmiHeader.biWidth=bmp->bmWidth;
+	bmi.bmiHeader.biHeight=bmp->bmHeight;
+	bmi.bmiHeader.biPlanes=bmp->bmPlanes;
+	bmi.bmiHeader.biCompression=BI_RGB;
+	bmi.bmiHeader.biClrImportant=0;
+	bmi.bmiHeader.biClrUsed=0;
+	bmi.bmiHeader.biSizeImage = ((bmi.bmiHeader.biWidth * 32 +31)& ~31) /8 * bmi.bmiHeader.biHeight;
 
-		gsdata=newgif(&gifimage,bmp_header->biWidth,bmp_header->biHeight,colortable,bgindex);
-		if(gifimage!=0){
-			char *pixels=0;
-			int glen=0;
-			pixels=malloc(bmp_header->biWidth*bmp_header->biHeight);
-			if(pixels!=0){
-				int h=bmp_header->biHeight;
+	if(GetDIBits(hdc,hbmp,0,(WORD)bmp->bmHeight,data,&bmi,DIB_RGB_COLORS)){
+			*pixels=malloc(w*h);
+			if(*pixels!=0){
 				int row_width;
-				if(h==0)
-					h=1;
-				row_width=bmp_header->biSizeImage/h;
-				if(bmp_header->biSizeImage != (3*bmp_header->biWidth*bmp_header->biHeight)){
-					int i;
-					i++;
-				}
-				dither(data,bmp_header->biWidth,bmp_header->biHeight,row_width,pixels);
-				putgif(gsdata,pixels);
-				free(pixels);
+				row_width=bmp->bmWidthBytes;
+				downsample(data,w,h,row_width,*pixels);
+				result=TRUE;
 			}
-			glen=endgif(gsdata);
-			{
-				FILE *f;
-				f=fopen("b:\\test.gif","wb");
-				if(f!=0){
-					fwrite(gifimage,1,glen,f);
-					fclose(f);
-				}
-			}
-
-			if(gifimage!=0)
-				free(gifimage);
-
-		}
 	}
 	if(data)
 		GlobalFree((HGLOBAL)data);
 	return result;
 }
-int take_snap(HWND hwnd)
+int take_snap(HWND hwnd,unsigned char **pixels,int w,int h)
 {
 	int result=FALSE;
 	HDC hscreen;
@@ -390,22 +337,21 @@ int take_snap(HWND hwnd)
 		if(htarget!=0){
 			RECT rect;
 			HBITMAP hbmp;
-			int w,h;
-			GetClientRect(hwin,&rect);
-			w=rect.right-rect.left-1;
-			h=rect.bottom-rect.top-1;
 			hbmp=CreateCompatibleBitmap(hscreen,w,h);
 			if(hbmp!=0){
-				PBITMAPINFO pBitmapInfo;
+				//PBITMAPINFO pBitmapInfo;
+				BITMAP bmp;
 				HBITMAP hold;
 				hold=SelectObject(htarget,hbmp);
 				BitBlt(htarget, 0, 0, w, h, hscreen, 0, 0, SRCCOPY);
 				SelectObject(htarget, hold);
-				pBitmapInfo=CreateBitmapInfoStruct(hbmp);
-				save_gif(htarget,hbmp,pBitmapInfo);
-				CreateBMPFile(("b:\\picture.bmp"), pBitmapInfo, hbmp, htarget);
+				if(GetObject(hbmp,sizeof(BITMAP),&bmp)){
+				//pBitmapInfo=CreateBitmapInfoStruct(hbmp);
+					if(grab_pixels(htarget,hbmp,&bmp,pixels,w,h))
+						result=TRUE;
+				}
+				//CreateBMPFile(("b:\\picture.bmp"), pBitmapInfo, hbmp, htarget);
 				DeleteObject(hbmp);
-				result=TRUE;
 			}
 			DeleteDC(htarget);
 		}
@@ -413,12 +359,65 @@ int take_snap(HWND hwnd)
 	}
 	return result;
 }
+int animate(HWND hwnd,int step)
+{
+	static unsigned char *gifimage=0;
+	static void *gsdata=0;
+	static int w=0,h=0;
+	if(step==0){
+		RECT rect={0};
+		GetClientRect(hwnd,&rect);
+		w=rect.right-rect.left-1;
+		h=rect.bottom-rect.top-1;
+
+		gsdata=newgif(&gifimage,w,h,colortable,0);
+		if(gsdata!=0){
+			animategif(gsdata,0,5,0,2);
+		}
+	}
+	else if(step==1){
+		unsigned char *pixels=0;
+		if(gifimage!=0){
+			take_snap(hwnd,&pixels,w,h);
+			if(pixels!=0){
+				int len;
+				char str[80];
+				len=putgif(gsdata,pixels);
+				_snprintf(str,sizeof(str),"%i (0x%08X)",len,len);
+				str[sizeof(str)-1]=0;
+				SetWindowText(hwnd,str);
+				free(pixels);
+			}
+		}
+	}
+	else if(step==2){
+		if(gsdata!=0){
+			char str[80];
+			int glen=0;
+			glen=endgif(gsdata);
+			_snprintf(str,sizeof(str),"DONE %i (0x%08X)",glen,glen);
+			SetWindowText(hwnd,str);
+			{
+				FILE *f;
+				f=fopen("b:\\test.gif","wb");
+				if(f!=0){
+					fwrite(gifimage,1,glen,f);
+					fclose(f);
+				}
+			}
+
+		}
+		if(gifimage!=0)
+			free(gifimage);
+	}
+
+}
 BOOL CALLBACK gifcap(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 {
 	static HRGN hregion=0;
 	{
 		static DWORD tick=0;
-		print_msg(msg,lparam,wparam,hwnd);
+		//print_msg(msg,lparam,wparam,hwnd);
 	}
 	switch(msg){
 	case WM_INITDIALOG:
@@ -431,12 +430,13 @@ BOOL CALLBACK gifcap(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 			clear_window(hwnd,&hregion);
 			SetWindowRgn(hwnd,hregion,TRUE);
 			SetWindowPos(hwnd,HWND_TOPMOST,0,0,0,0,SWP_NOMOVE|SWP_NOSIZE);
+			create_ctable();
 		}
 		break;
 	case WM_APP:
 		switch(wparam){
 		case '1':
-			take_snap(hwnd);
+			animate(hwnd,lparam);
 			printf("made it\n");
 			break;
 		}
@@ -464,15 +464,26 @@ int key_thread()
 {
 	while(TRUE){
 		Sleep(100);
-		if(GetAsyncKeyState('1')&0x8001){
+		if(GetAsyncKeyState(VK_SHIFT)&0x8001){
+			int i;
+			Beep(1000,100);
 			PostMessage(ghdlg,WM_APP,'1',0);
+			Sleep(33);
+			for(i=0;i<5000;i++){
+				PostMessage(ghdlg,WM_APP,'1',1);
+				Sleep(33);
+				if(GetAsyncKeyState(VK_CONTROL)&0x8001)
+					break;
+			}
+			PostMessage(ghdlg,WM_APP,'1',2);
+			Beep(800,100);
 			printf("1 presed\n");
 		}
 	}
 }
 int CALLBACK WinMain(HINSTANCE hinstance,HINSTANCE hprevinstance,char *cmdline,int showcmd)
 {
-	open_console();
+	//open_console();
 	_beginthread(key_thread,0,0);
 	return DialogBoxParam(hinstance,MAKEINTRESOURCE(IDD_DIALOG1),NULL,gifcap,NULL);
 }
